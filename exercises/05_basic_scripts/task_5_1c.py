689bdc79901d1e02161def85ae9cff3b651707de
EvgenHome <johnevgen@mail.ru>
2022-01-30 14:32:36 +0700

    Сделал 5.1c

--- exercises/05_basic_scripts/task_5_1c.py
+++ exercises/05_basic_scripts/task_5_1c.py
@@ -49,3 +49,9 @@ london_co = {
         "routing": True,
     },
 }
+
+router = input('Введите имя устройства: ')
+option = input(f"Введите имя параметра {list(london_co[router].keys())}: ")
+
+print(london_co[router].get(option,'Такого параметра нет'))
+
