b6928055e83509b1e9be724364bd310e9406ae29
EvgenHome <johnevgen@mail.ru>
2022-01-31 19:43:11 +0700

    Сделал 5.3а

--- exercises/05_basic_scripts/task_5_3a.py
+++ exercises/05_basic_scripts/task_5_3a.py
@@ -31,12 +31,11 @@ switchport_all_modes.append(access_template[0].split()[2])
 switchport_all_modes.append(trunk_template[1].split()[2])
 dict_switch_mode = {'access':access_template, 'trunk':trunk_template}
 
-access_banner = 'Введите номер VLAN:'
-trunk_banner = 'Введите разрешенные VLANы:'
+banner_dict = {'access':'Введите номер VLAN:','trunk':'Введите разрешенные VLANы:'}
 
 switchport_mode = input(f"Введите режим работы интерфейса {switchport_all_modes}: ")
 interface = input('Введите тип и номер интерфейса в формтате Fa0/1:  ')
-vlans = input(f"{switchport_mode}_banner")
+vlans = input(banner_dict[switchport_mode])
 
 print(f"interface {interface}")
 print('\n'.join(dict_switch_mode[switchport_mode]).format(vlans))
