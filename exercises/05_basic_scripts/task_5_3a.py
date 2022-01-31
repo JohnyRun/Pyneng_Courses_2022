79bb9622b601ff3858eb3051a1660e27340e1d15
evgeniy.kiselev <evgeniy.kiselev@eltex.loc>
2022-01-31 18:18:15 +0700

    Частично 5.3a

--- exercises/05_basic_scripts/task_5_3a.py
+++ exercises/05_basic_scripts/task_5_3a.py
@@ -25,3 +25,18 @@ trunk_template = [
     "switchport mode trunk",
     "switchport trunk allowed vlan {}",
 ]
+
+switchport_all_modes = []
+switchport_all_modes.append(access_template[0].split()[2])
+switchport_all_modes.append(trunk_template[1].split()[2])
+dict_switch_mode = {'access':access_template, 'trunk':trunk_template}
+
+access_banner = 'Введите номер VLAN:'
+trunk_banner = 'Введите разрешенные VLANы:'
+
+switchport_mode = input(f"Введите режим работы интерфейса {switchport_all_modes}: ")
+interface = input('Введите тип и номер интерфейса в формтате Fa0/1:  ')
+vlans = input(f"{switchport_mode}_banner")
+
+print(f"interface {interface}")
+print('\n'.join(dict_switch_mode[switchport_mode]).format(vlans))
