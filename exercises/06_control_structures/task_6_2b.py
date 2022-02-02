41e5e6a68a786919030485242458cca5539888d5
EvgenHome <johnevgen@mail.ru>
2022-02-02 21:49:27 +0700

    Сделал 6.2b

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -12,3 +12,38 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+while True:
+    ip_address = input('Введите ip-адрес в формате 10.0.1.1: ')
+    ip_address_octets_list = ip_address.split('.')
+    new_ip_address_octets_list = []
+    true_flag = False
+    try:
+        if len(ip_address_octets_list) != 4:
+            true_flag = True
+        elif ip_address.count('.') != 3:
+            true_flag = True
+        for ip in ip_address_octets_list:
+            if int(ip) not in range(0,256):
+                true_flag = True
+                break
+            else:
+                new_ip_address_octets_list.append(int(ip))
+        if not true_flag:
+            if int(new_ip_address_octets_list[0]) in range(1,224):
+                print('unicast')
+            elif int(new_ip_address_octets_list[0]) in range(224,240):
+                print('multicast')
+            elif new_ip_address_octets_list[0] == '255' and new_ip_address_octets_list[1] == '255' and new_ip_address_octets_list[2] == '255' and new_ip_address_octets_list[3] == '255':
+                print('local broadcast')
+            elif new_ip_address_octets_list[0] == '0' and new_ip_address_octets_list[1] == '0' and new_ip_address_octets_list[2] == '0' and new_ip_address_octets_list[3] == '0':
+                print('unassigned')
+            else:
+                print('unused')
+    except ValueError:
+        true_flag = True
+
+    if true_flag:
+        print('Неправильный IP-адрес')
+    else:
+        break
