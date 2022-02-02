6fcf7dc189e5616c0e67e8a6e3190b6bf267e3bd
evgeniy.kiselev <evgeniy.kiselev@eltex.loc>
2022-02-02 17:47:14 +0700

    Сделал 6.2

--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -12,3 +12,24 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip_address = input('Введите ip-адрес в формате 10.0.1.1: ')
+
+ip_address_octets_list = ip_address.split('.')
+
+if int(ip_address_octets_list[0]) in range(1,224):
+    print('unicast')
+elif int(ip_address_octets_list[0]) in range(224,240):
+    print('multicast')
+elif ip_address_octets_list[0] == '255' and ip_address_octets_list[1] == '255' and ip_address_octets_list[2] == '255' and ip_address_octets_list[3] == '255':
+    print('local broadcast')
+elif ip_address_octets_list[0] == '0' and ip_address_octets_list[1] == '0' and ip_address_octets_list[2] == '0' and ip_address_octets_list[3] == '0':
+    print('unassigned')
+else:
+    print('unused')
+
+
+
+
+
+
