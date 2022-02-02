a18a1e028ad646f790a73f4ac73bc45c2ccedfba
EvgenHome <johnevgen@mail.ru>
2022-02-02 21:43:54 +0700

    Сделал 6.2a

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -22,22 +22,34 @@ ip_address = input('Введите ip-адрес в формате 10.0.1.1: ')
 
 ip_address_octets_list = ip_address.split('.')
 new_ip_address_octets_list = []
+true_flag = False
 
-if len(ip_address_octets_list) == 4 and ip_address_octets_list.count('.') == 3:
+try:
+    if len(ip_address_octets_list) != 4:
+        true_flag = True
+    elif ip_address.count('.') != 3:
+        true_flag = True
     for ip in ip_address_octets_list:
-        try:
+        if int(ip) not in range(0,256):
+            true_flag = True
+            break
+        else:
             new_ip_address_octets_list.append(int(ip))
-        except ValueError:
-            print('Неправильный IP-адрес')
-    if int(ip_address_octets_list[0]) in range(1,224):
-        print('unicast')
-    elif int(ip_address_octets_list[0]) in range(224,240):
-        print('multicast')
-    elif ip_address_octets_list[0] == '255' and ip_address_octets_list[1] == '255' and ip_address_octets_list[2] == '255' and ip_address_octets_list[3] == '255':
-        print('local broadcast')
-    elif ip_address_octets_list[0] == '0' and ip_address_octets_list[1] == '0' and ip_address_octets_list[2] == '0' and ip_address_octets_list[3] == '0':
-        print('unassigned')
-    else:
-        print('unused')
-else:
+
+    if not true_flag:
+        if int(new_ip_address_octets_list[0]) in range(1,224):
+            print('unicast')
+        elif int(new_ip_address_octets_list[0]) in range(224,240):
+            print('multicast')
+        elif new_ip_address_octets_list[0] == '255' and new_ip_address_octets_list[1] == '255' and new_ip_address_octets_list[2] == '255' and new_ip_address_octets_list[3] == '255':
+            print('local broadcast')
+        elif new_ip_address_octets_list[0] == '0' and new_ip_address_octets_list[1] == '0' and new_ip_address_octets_list[2] == '0' and new_ip_address_octets_list[3] == '0':
+            print('unassigned')
+        else:
+            print('unused')
+
+except ValueError:
+    true_flag = True
+
+if true_flag:
     print('Неправильный IP-адрес')
