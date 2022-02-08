6bc864eb08fe7556410f5eb7ffd37927c759f52d
evgeniy.kiselev <evgeniy.kiselev@eltex.loc>
2022-02-08 16:52:43 +0700

    Сделал 7.1

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -14,3 +14,13 @@ Outbound Interface    FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+with open('ospf.txt') as file:
+    for line in file:
+        line = line.split()
+        print("Prefix {:>27}".format(line[1]))
+        print('AD/Metric {:>18}'.format(line[2].strip('[]')))
+        print('Next-Hop {:>22}'.format(line[4].strip(',')))
+        print('Last update {:>15}'.format(line[5].strip(',')))
+        print('Outbound Interface {:>18}'.format(line[-1]))
+        print('\n')
