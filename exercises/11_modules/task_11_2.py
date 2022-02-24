02a09578a8f835d0f344d2cb5537080fe9298c05
EvgenHome <johnevgen@mail.ru>
2022-02-24 17:43:24 +0700

    Сделал 11.2

--- exercises/11_modules/task_11_2.py
+++ exercises/11_modules/task_11_2.py
@@ -28,9 +28,24 @@ Cгенерировать топологию, которая соответст�
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+from task_11_1 import parse_cdp_neighbors
+from pprint import pprint
+
 infiles = [
     "sh_cdp_n_sw1.txt",
     "sh_cdp_n_r1.txt",
     "sh_cdp_n_r2.txt",
     "sh_cdp_n_r3.txt",
 ]
+
+def create_network_map(filenames):
+    result_dict = {}
+    for config_file in filenames:
+        with open(config_file) as f:
+            file = f.read()
+            result_dict.update(parse_cdp_neighbors(file))
+    return result_dict
+
+if __name__ == "__main__":
+    pprint(create_network_map(infiles))
