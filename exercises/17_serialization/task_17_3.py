a7fc65f87e219fc1b2d52eba4202604f07f2a62a
evgeniy.kiselev <evgeniy.kiselev@eltex.loc>
2022-03-24 17:58:29 +0700

    Сделал 17.3

--- exercises/17_serialization/task_17_3.py
+++ exercises/17_serialization/task_17_3.py
@@ -24,3 +24,26 @@ R6           Fa 0/2          143           R S I           2811       Fa 0/0
 
 Проверить работу функции на содержимом файла sh_cdp_n_sw1.txt
 """
+
+import re
+
+def parse_sh_cdp_neighbors(filename):
+    result_dict = {}
+    include_dict1 = {}
+    regex = (r'(?P<device>\S+)>[\S+ +]+'
+             r'|(?P<device_id>\S+)\s+(?P<Local_int>\S+ \S+)\s+\d+\s+\S+\s+\S+\s+\S+\s+\S+\s+(?P<port_id>\S+ \S+)')
+    match = re.findall(regex,filename)
+    if match:
+        for line in match:
+           include_dict2 = {}
+           if line[0]:
+               hostname = line[0]
+           else:
+               include_dict2[line[1]] = line[3]
+               include_dict1[line[2]] = include_dict2
+               result_dict[hostname] = include_dict1
+    return result_dict
+
+if __name__ == '__main__':
+    with open('sh_cdp_n_sw1.txt','r') as file:
+        parse_sh_cdp_neighbors(file.read())
