aaff49920294fbbae6057bcfadefa9ea413f8abf
EvgenHome <johnevgen@mail.ru>
2022-04-20 15:14:27 +0700

    Сделал 21.1a

--- exercises/21_textfsm/task_21_1a.py
+++ exercises/21_textfsm/task_21_1a.py
@@ -16,3 +16,16 @@
 Проверить работу функции на выводе команды output/sh_ip_int_br.txt
 и шаблоне templates/sh_ip_int_br.template.
 """
+
+from pprint import pprint
+import textfsm
+
+def parse_output_to_dict(template, command_output):
+    with open(template) as f, open(command_output) as output:
+        re_table = textfsm.TextFSM(f)
+        return re_table.ParseTextToDicts(output.read())
+
+# вызов функции должен выглядеть так
+if __name__ == "__main__":
+    result = parse_output_to_dict("templates/sh_ip_int_br.template", 'output/sh_ip_int_br.txt')
+    pprint(result)
