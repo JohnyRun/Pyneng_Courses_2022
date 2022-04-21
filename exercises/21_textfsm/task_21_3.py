60272733be942655a0d7188572ea54e676790933
EvgenHome <johnevgen@mail.ru>
2022-04-21 12:50:34 +0700

    Сделал 21.3

--- exercises/21_textfsm/task_21_3.py
+++ exercises/21_textfsm/task_21_3.py
@@ -20,3 +20,23 @@
 
 Проверить работу функции на примере вывода команды sh ip int br.
 """
+from pprint import pprint
+from textfsm import clitable
+import textfsm
+
+attributes = {'Command':'sh ip int br', 'Vendor':'cisco_ios'}
+
+def parse_command_dynamic(command_output, attributes_dict, templ_path='templates', index_file='index'):
+    result_list = []
+    with open(command_output,'r') as f:
+        output_sh_ip_int_br = f.read()
+    cli_table = clitable.CliTable(index_file, templ_path)
+    cli_table.ParseCmd(output_sh_ip_int_br,attributes_dict)
+    data_rows = [list(row) for row in cli_table]
+    for row in data_rows:
+        result_list.append(dict(zip(cli_table.header, row)))
+    return result_list
+
+
+if __name__ == "__main__":
+    pprint(parse_command_dynamic('output/sh_ip_int_br.txt', attributes))
