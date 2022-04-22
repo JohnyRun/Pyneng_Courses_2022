1780b5c7a711f1c60c190486b9d155e031d16ebb
EvgenHome <johnevgen@mail.ru>
2022-04-22 11:21:19 +0700

    Сделал 22.1b

--- exercises/22_oop_basics/task_22_1b.py
+++ exercises/22_oop_basics/task_22_1b.py
@@ -63,3 +63,27 @@ topology_example = {
     ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
     ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
 }
+
+from pprint import pprint
+
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+    def _normalize(self,topology_dict):
+        new_dict = topology_dict.copy()
+        for key, value in topology_dict.items():
+            if topology_dict.get(value) == key and new_dict.get(value) == key:
+                del new_dict[key]
+        return new_dict
+    def delete_link(self,link1,link2):
+        if self.topology.get(link1) == link2:
+            del self.topology[link1]
+        elif self.topology.get(link2) == link1:
+            del self.topology[link2]
+        else:
+            print('Такого соединения нет')
+
+if __name__ == "__main__":
+    top = Topology(topology_example)
+    top.delete_link(('SW2', 'Eth0/11'), ('R2', 'Eth0/1'))
+    pprint(top.topology)
