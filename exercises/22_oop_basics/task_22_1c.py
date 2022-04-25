900a8a10ec6dd14b6473273a623774b1dc408aaf
EvgenHome <johnevgen@mail.ru>
2022-04-25 13:52:14 +0700

    Сделал 22.1c

--- exercises/22_oop_basics/task_22_1c.py
+++ exercises/22_oop_basics/task_22_1c.py
@@ -35,6 +35,7 @@ In [5]: t.delete_node('SW1')
 Такого устройства нет
 
 """
+from pprint import pprint
 
 topology_example = {
     ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
@@ -47,3 +48,37 @@ topology_example = {
     ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
     ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
 }
+
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+    def _normalize(self,topology_dict):
+        new_dict = topology_dict.copy()
+        for key, value in topology_dict.items():
+            if topology_dict.get(value) == key and new_dict.get(value) == key:
+                del new_dict[key]
+        return new_dict
+    def delete_link(self,link1,link2):
+        if self.topology.get(link1) == link2:
+            del self.topology[link1]
+        elif self.topology.get(link2) == link1:
+            del self.topology[link2]
+        else:
+            print('Такого соединения нет')
+    def delete_node(self,device):
+        temp_topology = self.topology.copy()
+        flag = False
+        for item in temp_topology.items():
+            if device == item[0][0] or device == item[1][0]:
+                del self.topology[item[0]]
+                flag = True
+        if not flag:
+            print('Такого соединения нет')
+
+
+
+if __name__ == "__main__":
+    top = Topology(topology_example)
+    pprint(top.topology)
+    top.delete_node('SW1')
+    pprint(top.topology)
