9f5ffde0131ef4cfc95e9406392b1af8cb020a36
EvgenHome <johnevgen@mail.ru>
2022-04-27 16:01:12 +0700

    Сделал 23.3

--- exercises/23_oop_special_methods/task_23_3.py
+++ exercises/23_oop_special_methods/task_23_3.py
@@ -58,6 +58,7 @@ Out[9]:
 In [10]: t2.topology
 Out[10]: {('R1', 'Eth0/4'): ('R7', 'Eth0/0'), ('R1', 'Eth0/6'): ('R9', 'Eth0/0')}
 """
+from pprint import pprint
 
 topology_example = {
     ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
@@ -75,3 +76,50 @@ topology_example2 = {
     ("R1", "Eth0/4"): ("R7", "Eth0/0"),
     ("R1", "Eth0/6"): ("R9", "Eth0/0"),
 }
+
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+    def _normalize(self,topology_dict):
+        new_dict = topology_dict.copy()
+        for key, value in topology_dict.items():
+            if topology_dict.get(value) == key and new_dict.get(value) == key:
+                del new_dict[key]
+        return new_dict
+    def delete_link(self,link1,link2):
+        if self.topology.get(link1) == link2:
+            del self.topology[link1]
+        elif self.topology.get(link2) == link1:
+            del self.topology[link2]
+        else:
+            print('Такого соединения нет')
+    def delete_node(self,device):
+        temp_topology = self.topology.copy()
+        flag = False
+        for item in temp_topology.items():
+            if device == item[0][0] or device == item[1][0]:
+                del self.topology[item[0]]
+                flag = True
+        if not flag:
+            print('Такого соединения нет')
+    def add_link(self,link1,link2):
+        if self.topology.get(link1) == link2:
+            print('Такое соединение существует')
+        elif link1 in self.topology.keys() or link2 in self.topology.keys():
+            print('Cоединение с одним из портов существует')
+        elif link1 in self.topology.values() or link2 in self.topology.values():
+            print('Cоединение с одним из портов существует')
+        else:
+            self.topology[link1] = link2
+    def __add__(self, other):
+        sum_topology = dict(self.topology)
+        sum_topology.update(dict(other.topology))
+        return Topology(sum_topology)
+
+if __name__ == '__main__':
+    t1 = Topology(topology_example)
+    t2 = Topology(topology_example2)
+    t3 = t1+t2
+    pprint(t1.topology)
+    pprint(t2.topology)
+    pprint(t3.topology)
