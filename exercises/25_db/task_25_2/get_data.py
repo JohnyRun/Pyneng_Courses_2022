dd4029d64fb2d77d2bad4d2e50cdbd4df49f32c8
evgeniy.kiselev <evgeniy.kiselev@eltex.loc>
2022-05-16 16:54:12 +0700

    Сделал 25.2

--- exercises/25_db/task_25_2/get_data.py
+++ exercises/25_db/task_25_2/get_data.py
@@ -1,19 +1,36 @@
+#! /usr/bin/env python3
+
 import sqlite3
 from sys import argv
+from pprint import pprint
 
-parameter = argv[0]
-value = argv[1]
 correct_parameters = ['mac', 'ip', 'vlan', 'interface', 'switch']
 
 def request_to_table(param=None,value=None):
+    connection = sqlite3.connect('dhcp_snooping.db')
+    cursor = connection.cursor()
     if param and value:
         if param in correct_parameters:
             try:
-                connection = sqlite3.connect('dhcp_snooping.db')
-                cursor = connection.cursor()
-                cursor.execute(f'select * from dhcp where {param} = "{value}"')
+                 cursor.execute(f'select * from dhcp where {param} = "{value}"')
+                 pprint(cursor.fetchall())
+                 print(value)
             except sqlite3.IntegrityError as e:
-                print(f'При выполнении запроса с параметрами: {param} и {value} возникла ошибка {e}')
+                  print(f'При выполнении запроса с параметрами: {param} и {value} возникла ошибка {e}')
         else:
             print('Данный параметр не поддерживается.')
-            print('Допустимые значения параметров: mac, ip, vlan, interface, switch')
+            print(f'Допустимые значения параметров: {correct_parameters}')
+    else:
+        cursor.execute('select * from dhcp')
+        pprint(cursor.fetchall())
+
+
+if __name__ == '__main__':
+    if len(argv) == 3:
+        param = argv[1]
+        value = argv[2]
+        request_to_table(param,value)
+    elif len(argv) == 1:
+        request_to_table()
+    elif len(argv) == 2 or len(argv) > 3:
+        print('Скрипт поддерживает 0 или 2 аргумента')
