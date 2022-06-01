182fa25014791131bf01f0353bb370c265148607
evgeniy.kiselev <evgeniy.kiselev@eltex.loc>
2022-06-01 16:22:32 +0700

    Сделал 25.3

---
+++ exercises/25_db/task_25_3/add_data.py
@@ -0,0 +1,71 @@
+import re
+import sqlite3
+import yaml
+from pprint import pprint
+import os
+from create_db import create_database
+
+def add_switch_information(switch_config_yaml, connection_name):
+    '''
+    :param configname: file.yaml
+    '''
+    print('Добавляю данные в таблицу switches')
+    with open(switch_config_yaml) as f:
+        switches_information = yaml.safe_load(f)
+        for switches in switches_information.values():
+            for key,value in switches.items():
+                tup = tuple([key,value])
+                try:
+                    query = '''insert into switches (hostname, location)
+                                       values (?, ?)'''
+                    connection_name.execute(query, tup)
+                    connection_name.commit()
+                except sqlite3.IntegrityError as e:
+                    print(f'При добавлении данных {tup} Возникла ошибка: {e}')
+
+def dhcp_snooping_information(dhcp_snooping_output, connection_name):
+    print('Добавляю данные в таблицу dhcp')
+    cursor = connection_name.cursor()
+    result = []
+    mac_list_from_file = []
+    switch_name = dhcp_snooping_output.split('_')[0]
+    regex = re.compile(r'(\S+) +(\S+) +\d+ +\S+ +(\d+) +(\S+)')
+    with open(dhcp_snooping_output) as file:
+        for line in file.readlines():
+            match = regex.search(line)
+            if match:
+                temp_list = list(match.groups())
+                temp_list.append(switch_name)
+                result.append(tuple(temp_list))
+    for mac in result:
+        mac_list_from_file.append(mac[0])
+    for row in result:
+        try:
+            query = '''replace into dhcp (mac, ip, vlan, interface, switch, active)
+                               values (?, ?, ?, ?, ?, 1)'''
+            connection_name.execute(query, row)
+            connection_name.commit()
+        except sqlite3.IntegrityError as e:
+            print(f'При добавлении данных {row} Возникла ошибка: {e}')
+    for line in cursor.execute('select mac from dhcp'):
+        if line[0] not in mac_list_from_file:
+            connection_name.execute(f'update dhcp set active = "0" where mac="{line[0]}"')
+            connection_name.commit()
+
+
+if __name__ == '__main__':
+    db_filename = 'dhcp_snooping.db'
+    db_exists = os.path.exists(db_filename)
+    filenames = ['sw1_dhcp_snooping.txt', 'sw2_dhcp_snooping.txt', 'sw3_dhcp_snooping.txt']
+    new_filenames = ['new_data/sw1_dhcp_snooping.txt', 'new_data/sw2_dhcp_snooping.txt', 'new_data/sw3_dhcp_snooping.txt']
+    if db_exists:
+        connection = sqlite3.connect(db_filename)
+        cursor = connection.cursor()
+        #for file in filenames:
+            #dhcp_snooping_information(file,connection)
+        dhcp_snooping_information(filenames[2], connection)
+        cursor.execute('select * from dhcp')
+        pprint(cursor.fetchmany(13))
+    else:
+        print('База данных не существует. Перед добавлением данных, ее надо создать')
+        create_database('dhcp_snooping.db')
